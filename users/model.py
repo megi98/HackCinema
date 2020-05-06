90bc45f6f6d09484177d2a83be8ece4c302d86ca
Mariya-Magdalena <60431074+megi98@users.noreply.github.com>
2020-05-06 23:24:11 +0300

    add hash_password method

--- users/model.py
+++ users/model.py
@@ -1,3 +1,6 @@
+import hashlib
+
+
 SPECIAL_SYMBOLS = '`~!@#$%^&*()_-+=|:;"<>.?/'
 
 
@@ -9,12 +12,12 @@ class UserModel:
 
 	@staticmethod
 	def validate(email, password):
-		if self.email.count('@') != 1:
+		if email.count('@') != 1:
 			raise ValueError('Incorrect email.')
 
-		inex_at = email.index('@')
+		index_at = email.index('@')
 
-		if self.email.count('.') != 1:
+		if email.count('.') != 1:
 			raise ValueError('Incorrect email.')
 
 		index_period = email.index('.')
@@ -22,11 +25,11 @@ class UserModel:
 		if index_at > index_period:
 			raise ValueError('Incorrect email.')
 
-		if len(self.password) < 8:
+		if len(password) < 8:
 			raise ValueError('Invalid password.')
 
 		count_special_symbols = 0
-		for symbol in self.password:
+		for symbol in password:
 			if symbol in SPECIAL_SYMBOLS:
 				count_special_symbols += 1
 
@@ -34,10 +37,15 @@ class UserModel:
 			raise ValueError('Invalid password.')
 
 		count_capital_letter = 0
-		for symbol in self.password:
+		for symbol in password:
 			if symbol.isupper():
 				count_capital_letter += 1
 
 		if count_capital_letter < 1:
 			raise ValueError('Invalid password.')
-				
+
+
+	@staticmethod
+	def hash_password(password):
+		return hashlib.sha512(password.encode()).hexdigest()
+	
