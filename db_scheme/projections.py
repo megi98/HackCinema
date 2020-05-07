ad342cb11170befd61ba2de0fff6fcee2ddc672a
Mariya-Magdalena <60431074+megi98@users.noreply.github.com>
2020-05-08 00:33:36 +0300

    Update projections.py

--- db_scheme/projections.py
+++ db_scheme/projections.py
@@ -1,9 +1,10 @@
 CREATE_PROJECTIONS = '''
     CREATE TABLE IF NOT EXISTS projections (
-        id integer PRIMARY KEY NOT NULL,
-        movie_id FOREIGN KEY NOT NULL REFERENCES movies(id),
+        id INTEGER PRIMARY KEY NOT NULL,
+        movie_id INTEGER NOT NULL,
         type VARCHAR(5),
         date DATE,
-        time VARCHAR(5)
+        time VARCHAR(5),
+        FOREIGN KEY (movie_id) REFERENCES movies(id)
     );
 '''
