6d5b6dbfcfcbbecbf43652183130fc162122da4b
Mariya-Magdalena <60431074+megi98@users.noreply.github.com>
2020-05-08 00:34:26 +0300

    Update reservations.py

--- db_scheme/reservations.py
+++ db_scheme/reservations.py
@@ -1,9 +1,11 @@
 CREATE_RESERVATIONS = '''
     CREATE TABLE IF NOT EXISTS reservations (
         id integer PRIMARY KEY NOT NULL,
-        user_id INTEGER FOREIGN KEY REFERENCES users(id),
-        projection_id INTEGER FOREIGN KEY REFERENCES projections(id),
+        user_id INTEGER,
+        projection_id INTEGER,
         row INTEGER,
-        col INTEGER
+        col INTEGER,
+        FOREIGN KEY (user_id) REFERENCES users(id),
+        FOREIGN KEY (projection_id) REFERENCES projections(id) 
     );
 '''
