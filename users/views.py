00b17f7b1f55174e3a5e765676652dbd2e6a1216
Mariya-Magdalena <60431074+megi98@users.noreply.github.com>
2020-05-08 00:41:00 +0300

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,7 +1,26 @@
 from controllers import UserController
 
 
-
 class UserViews:
 	def __init__(self):
 		self.controller = UserController()
+
+
+	def welcome(self):
+		print('Welcome in HackCinema! Please enter your data to login:')
+		email = input('Email: ')
+		password = input('Password: ')
+
+		user = self.controller.get_user_by_id(email)
+
+		if user == 'No user with this data. Please sign up.':
+			print(user)
+			print('Enter your data: ')
+			email = input('Email: ')
+			password = input('Password: ')
+			self.controller.create_user(email, password)
+			print('Thank you for your registration!')
+
+		print('You are logged in our system now :)')
+		return user
+	
