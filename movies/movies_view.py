e83262d7d42fe337b315025cf25c450f6a8c2418
Mariya-Magdalena <60431074+megi98@users.noreply.github.com>
2020-05-08 16:28:23 +0300

    Update movies_view.py

--- movies/movies_view.py
+++ movies/movies_view.py
@@ -13,31 +13,31 @@ class MovieView:
 
 	def show_all_movies(self):
 		movies = self.controller.show_all_movies()
+		print('Current movies:')
 
 		for movie in movies:
-			print(f'[{movie[0]}] - {movie[1]} ({movie[2]})\n')
+			print(f'[{movie[0]}] - {movie[1]} ({movie[2]})')
 
 
 	def show_projections_by_movie_id(self, movie_id):
 		projections = self.controller.get_projections_by_movie_id(movie_id)
+		print(f"Projections for movie '{self.get_movie_by_id(movie_id)}'")
 
 		for projection in projections:
-			print(f'{projection[0]} | {projection[1]} | {projection[2]} | {projection[3]} | {projection[4]}\n')
+			print(f'[{projection[0]}] - {projection[3]} {projection[4]} ({projection[2]})')
 
 
 	def show_projections_by_movie_id_and_date(self, movie_id, date):
 		projections = self.controller.get_projections_by_movie_id_and_date(movie_id, date)
+		print(f"Projections for movie '{self.get_movie_by_id(movie_id)}' on date {date}:")
 
 		for projection in projections:
-			print(f'{projection[0]} | {projection[1]} | {projection[2]} | {projection[3]} | {projection[4]}\n')
+			print(f'[{projection[0]}] - {projection[4]} ({projection[2]})')
 
 
 	def show_all_projections(self):
 		projections = self.controller.get_all_projections()
+		print('All projections:')
 
 		for projection in projections:
-			print(f'{projection[0]} | {projection[1]} | {projection[2]} | {projection[3]} | {projection[4]}\n')
-
-
-mv = MovieView()
-mv.show_all_movies()
+			print(f'[{projection[0]}] [{projection[1]}] - {projection[3]} {projection[4]} ({projection[2]})')
