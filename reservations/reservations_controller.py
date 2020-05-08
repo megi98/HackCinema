8d16478f0b1312e04f084e7403c7472fe7fbdffa
Mariya-Magdalena <60431074+megi98@users.noreply.github.com>
2020-05-08 16:30:53 +0300

    Update reservations_controller.py

--- reservations/reservations_controller.py
+++ reservations/reservations_controller.py
@@ -16,4 +16,15 @@ class ReservationController:
 
 	def get_taken_seats_for_projection(self, *, projection_id):
 		return self.gateway.get_taken_seats_for_projection(projection_id=projection_id)
+
+
+	def available_seats_count(self, *, projection_id):
+		return self.gateway.available_seats_count(projection_id=projection_id)
 		
+
+	def check_if_seat_is_available(self, *, projection_id, row, col):
+		return self.gateway.check_if_seat_is_available(projection_id=projection_id, row=row, col=col)
+
+
+	def check_if_seat_is_out_of_range(self, *, row, col):
+		return self.gateway.check_if_seat_is_out_of_range(row=row, col=col)
