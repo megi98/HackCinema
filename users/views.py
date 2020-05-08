489a03d55d73753148a8f35410b1a933c5b7221c
Mariya-Magdalena <60431074+megi98@users.noreply.github.com>
2020-05-08 16:25:25 +0300

    Update views.py

--- users/views.py
+++ users/views.py
@@ -4,6 +4,23 @@ from controllers import UserController
 class UserViews:
 	def __init__(self):
 		self.controller = UserController()
+		self.user_id = None
+
+
+	def get_user_id(self):
+		return self.user_id
+
+
+	def enter_data(self):
+		email = input('Email: ')
+		password = input('Password: ')
+
+		try:
+			self.controller.create_user(email, password)
+		except Exception as err:
+			print(f'{str(err)} Try again!')
+		finally:
+			return self.controller.get_user_by_id(email)
 
 
 	def welcome(self):
@@ -11,16 +28,15 @@ class UserViews:
 		email = input('Email: ')
 		password = input('Password: ')
 
-		user = self.controller.get_user_by_id(email)
+		self.user_id = self.controller.get_user_by_id(email)
 
-		if user == 'No user with this data. Please sign up.':
-			print(user)
+		if self.user_id == 'No user with this data. Please sign up.':
+			print(self.user_id)
 			print('Enter your data: ')
-			email = input('Email: ')
-			password = input('Password: ')
-			self.controller.create_user(email, password)
+			self.user_id = self.enter_data()
+			while type(self.user_id) is str:
+				self.user_id = self.enter_data()
 			print('Thank you for your registration!')
 
 		print('You are logged in our system now :)')
-		return user
-	
+
