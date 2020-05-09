81bf77064278f76d137e3c428e4800f3dbe02a93
Mariya-Magdalena <60431074+megi98@users.noreply.github.com>
2020-05-09 23:25:25 +0300

    Update reservarion_model.py

--- reservations/reservarion_model.py
+++ reservations/reservarion_model.py
@@ -1,7 +1,11 @@
 class ReservationModel:
-	def __init__(self, *, id, user_id, projection_id, row, col):
-		self.id = id
+	def __init__(self, *, user_id, projection_id, row, col):
 		self.user_id = user_id
 		self.projection_id = projection_id
 		self.row = row
 		self.col = col
+
+	@staticmethod
+	def validate(row, col):
+		if row > 10 or col > 10:
+			raise Exception('Lol...NO!')
