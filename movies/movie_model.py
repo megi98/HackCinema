fccaac43aa412337317d9c7b6717dc41aa69ae3c
Mariya-Magdalena <60431074+megi98@users.noreply.github.com>
2020-05-09 23:21:15 +0300

    Update movie_model.py

--- movies/movie_model.py
+++ movies/movie_model.py
@@ -1,5 +1,13 @@
 class MovieModel:
-	def __init__(self, *, id, name, rating):
-		self.id = id
+	def __init__(self, *, movie_id, name, rating):
+		self.movie_id = movie_id
 		self.name = name
 		self.rating = rating
+
+
+	def get_movie_id(self):
+		return self.movie_id
+
+
+	def get_movie_title(self):
+		return self.name
