19ad8d05cf700fb5eac05a1e2f57432006dbd7c2
Mariya-Magdalena <60431074+megi98@users.noreply.github.com>
2020-05-09 23:24:31 +0300

    Update movies_view.py

--- movies/movies_view.py
+++ movies/movies_view.py
@@ -6,38 +6,27 @@ class MovieView:
 		self.controller = MovieController()
 
 
-	def get_movie_by_id(self, id):
-		title = self.controller.get_movie_by_id(id)
-		return title
+	def insert_new_movie(self):
+		name = input('Title: ')
+		rating = int(input('Rating: '))
+		self.controller.add_movie(name=name, rating=rating)
+
+
+	def choose_movie_by_id(self):
+		movie_id = int(input('Choose the id for the movie you want: '))
+		movie = self.controller.get_movie_by_id(movie_id=movie_id)
+
+		while movie is False:
+			print('No movie with that id. Try again')
+			movie_id = int(input('> '))
+			movie = self.controller.get_movie_by_id(movie_id=movie_id)
+
+		print(f'Great choice!\nTitle: {movie[0]}\nRating: {movie[1]}')
 
 
 	def show_all_movies(self):
-		movies = self.controller.show_all_movies()
-		print('Current movies:')
+		movies = self.controller.get_all_movies()
+		print('Current movies:\n')
 
 		for movie in movies:
 			print(f'[{movie[0]}] - {movie[1]} ({movie[2]})')
-
-
-	def show_projections_by_movie_id(self, movie_id):
-		projections = self.controller.get_projections_by_movie_id(movie_id)
-		print(f"Projections for movie '{self.get_movie_by_id(movie_id)}'")
-
-		for projection in projections:
-			print(f'[{projection[0]}] - {projection[3]} {projection[4]} ({projection[2]})')
-
-
-	def show_projections_by_movie_id_and_date(self, movie_id, date):
-		projections = self.controller.get_projections_by_movie_id_and_date(movie_id, date)
-		print(f"Projections for movie '{self.get_movie_by_id(movie_id)}' on date {date}:")
-
-		for projection in projections:
-			print(f'[{projection[0]}] - {projection[4]} ({projection[2]})')
-
-
-	def show_all_projections(self):
-		projections = self.controller.get_all_projections()
-		print('All projections:')
-
-		for projection in projections:
-			print(f'[{projection[0]}] [{projection[1]}] - {projection[3]} {projection[4]} ({projection[2]})')
